8a2ee125863222ba888f2bd5a0efa18a581b983d
SagarNepali <nepalisagar23@gmail.com>
2018-04-01 16:39:50 +0545

    React router 4; nested routing;

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/components/App.jsx';
+import {BrowserRouter, Route, Link } from 'react-router-dom';
+import AppContainer from '../src/components/AppContainer/AppContainer.jsx';
+import Home from '../src/components/Home/Home.jsx';
+import About from '../src/components/About/About.jsx';
+import Repos from '../src/components/Repos/Repos.jsx';
 import './style.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(<AppContainer/>, document.getElementById('root'));
 
 
